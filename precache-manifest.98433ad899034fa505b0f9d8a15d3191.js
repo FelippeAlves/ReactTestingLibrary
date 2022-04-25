182a90ea1aeb9acde1c105f15b0d3a54fc00ada5
FelippeAlves <felippealves17@gmail.com>
2022-04-25 17:44:32 -0300

    Updates

---
+++ precache-manifest.98433ad899034fa505b0f9d8a15d3191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebf2b459dd4c04b4d54f6b63e35c69e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "5dec2567add5d2bfe320",
+    "url": "/react-app/static/css/main.c96d1127.chunk.css"
+  },
+  {
+    "revision": "54ebf21f82ecbd812890",
+    "url": "/react-app/static/js/2.5d385706.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-app/static/js/2.5d385706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dec2567add5d2bfe320",
+    "url": "/react-app/static/js/main.e6dd9fd8.chunk.js"
+  },
+  {
+    "revision": "a0869fd3ed4d6a5b8e7c",
+    "url": "/react-app/static/js/runtime-main.d74d1e89.js"
+  }
+]);
